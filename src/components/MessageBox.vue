2ee362b47ee40dcc9dd79eb95e2fd730c05077a3
Hivenet-Nolan <48387251+Edofo@users.noreply.github.com>
2023-12-13 09:21:25 +0100

    Create MessageBox.vue

---
+++ src/components/MessageBox.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { defineProps } from "vue"
+
+const props = defineProps({
+    message: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    }
+})
+
+const getPictoFromType = (type: string) => {
+    switch (type) {
+        case "error":
+            return "fas fa-exclamation-triangle"
+        case "success":
+            return "fas fa-check-circle"
+        case "info":
+            return "fas fa-info-circle"
+        default:
+            return "fas fa-exclamation-triangle"
+    }
+}
+</script>
+
+<template>
+    <div class="message-box">
+        <i class="picto" :class="getPictoFromType(props.type)" />
+        <p>{{ props.message }}</p>
+    </div>
+</template>
+
+<style scoped></style>
